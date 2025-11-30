7da2c69e25149409748aee2a3bde8e17de44dd31
Luuannn <luan.anderson.a.vieira@gmail.com>
2025-11-30 04:29:31 -0300

    BST finalizada

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -9,6 +9,10 @@ typedef struct Nó{
 
 void Inserir (nó **raiz, int n );
 void busca (nó *raiz, int n);
+void inOrder (nó *raiz);
+void preOrder (nó *raiz);
+void postOrder (nó *raiz);
+void remover (nó **raiz, int n); 
 
 int main (){
       
@@ -17,7 +21,7 @@ int main (){
 
     do
     {
-        printf("\n\n1.Inserir Valor\n2.Buscar Valor\n3.Busca\n4.Listagem\n0.Sair\n\n");
+        printf("1-Inserir Valor\n2-Buscar Valor\n3-Remover Valor\n4-Percorrer Arvore\n0-Sair\n\n");
         scanf("%d", &escolha);
 
         switch (escolha)
@@ -31,9 +35,27 @@ int main (){
             busca(raiz, n);
                 break;
         case 3:
-            
+            printf("Digiter o valor do nó a ser removido\n");scanf("%d", &n);
+            remover(&raiz, n);
               break;
         case 4:
+            printf("\n\n1-Pré-Ordem\n2-Em Ordemm\n3-Pós-Ordem\n");scanf("%d", &escolha);
+            switch (escolha)
+            {
+            case 1:
+                preOrder(raiz);
+                printf("\n");
+                break;
+            case 2:
+                inOrder(raiz);
+                printf("\n");
+                break;
+            case 3:
+                postOrder(raiz);
+                printf("\n");
+                break;
+            default:
+                printf("\nOpção Invalida\n"); }
              break;
         case 0:
              return 0;
@@ -61,17 +83,17 @@ void Inserir (nó **raiz, int n) {
      else if (n > (*raiz)->x)
         Inserir(&(*raiz)->Direita, n); 
      else {
-        printf("Valor Já existe na arvore"); //Caso seja um valor que ja está na arvore, nada é inserido
+        printf("Valor Já existe na arvore\n"); //Caso seja um valor que ja está na arvore, nada é inserido
         return; } 
     } 
 
 void busca (nó *raiz, int n){
      if (raiz == NULL){
-        printf("\nValor não se encontra na arvore\n");
+        printf("\nValor não se encontra na arvore\n\n");
         return;
      }
      if (raiz->x == n){
-         printf("\nO valor se encontra na arvore\n");
+         printf("\nO valor se encontra na arvore\n\n");
          return;
      }
      
@@ -79,4 +101,58 @@ void busca (nó *raiz, int n){
          busca(raiz->Esquerda, n);
      else if (n > raiz->x) 
          busca(raiz->Direita, n);
+}
+
+void preOrder (nó *raiz){
+    if(raiz){
+       printf("%d ", raiz->x);
+       preOrder(raiz->Esquerda);
+       preOrder(raiz->Direita);
+    }
+}
+void inOrder (nó *raiz){
+     if(raiz){
+        inOrder(raiz->Esquerda);
+        printf("%d ", raiz->x);
+        inOrder(raiz->Direita);
+     }
+}
+void postOrder (nó *raiz){
+    if(raiz){
+       postOrder(raiz->Esquerda);
+       postOrder(raiz->Direita);
+       printf("%d ", raiz->x);
+    }
+}
+
+void remover (nó **raiz, int n){
+      if(*raiz == NULL){
+        printf("Valor não está na arvore\n"); //Validação para ver se o valor esta na arvore
+        return;}
+
+      if (n < (*raiz)->x)                 //Preocurando o valor informado
+        remover(&(*raiz)->Esquerda, n); 
+     else if (n > (*raiz)->x)
+        remover(&(*raiz)->Direita, n);
+     else {                               //nó encontrado
+        
+         if((*raiz)->Esquerda == NULL && (*raiz)->Direita == NULL){ //se o nó a ser removido não tem nenhum filho
+            free(*raiz);
+            *raiz = NULL; }
+         else if ((*raiz)->Direita == NULL){ //se o nó tiver um filho e o filho tiver a direita
+            nó *aux = *raiz;
+            *raiz = (*raiz)->Esquerda;
+            free(aux); }
+         else if ((*raiz)->Esquerda == NULL){ //se o nó tiver um filho e o filho tiver a esquerda
+            nó *aux = *raiz;
+            *raiz = (*raiz)->Direita;
+            free(aux); }
+         else { //se o nó tiver 2 filhos
+            nó *aux = (*raiz)->Esquerda;
+            while (aux->Direita) {
+                aux = aux->Direita; }
+                (*raiz)->x = aux->x;
+                remover(&(*raiz)->Esquerda, aux->x);
+         }
+     }    
 }
